2ff400d7800578dec957e5d3d0461bb6850c8bae
Xavionstar <lincpough@gmail.com>
2023-01-19 17:58:00 -0800

    Added comments

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import fs from 'fs';
 
 
 inquirer
+//I added each of the questions for each section info
     .prompt([
         {
             type: 'input',
@@ -53,40 +54,43 @@ inquirer
             message: 'What is your email?',
         },
 
-        /* Pass your questions in here */
+        
     ])
+    //the create page method is the final large method that collects all the smaller functions
     .then((answers) => {
         createReadmePage(answers)
         console.log(answers)
-        // Use user feedback for... whatever!!
+        
     })
     .catch((error) => {
         if (error.isTtyError) {
             console.log(error)
-            // Prompt couldn't be rendered in the current environment
+            
         } else {
             console.log(error)
-            // Something else went wrong
+            
         }
     });
-
+//I wanted to create a cleaner code by using multiple methods and then making one larger method that calls the smaller ones
 function createReadmePage(answers) {
     const fd = fs.openSync("readme-output/README.md", "w");
     createTitle(fd, answers.title)
-    createTableContents(fd)
     createDescription(fd, answers.description)
+    createTableContents(fd)
     createInstallation(fd, answers.installation)
     createUsage(fd, answers.usage)
     createContribution(fd, answers.contribution)
     createLicense(fd, answers.licenses)
+    createTest(fd, answers.test)
     createQuestion(fd, answers.githubQuestion, answers.emailQuestion)
     fs.closeSync(fd)
 };
 
+//each of these is a smaller method that makes a section that has the heading and the information from the corresponding question
 function createTitle(fd, title) {
     fs.writeSync(fd, `# ${title}\n\n`)
 }
-
+//the table of contents works basically like a link but with unique syntax for markdown
 function createTableContents(fd) {
     fs.writeSync(fd, "## Table of Contents \n")
     fs.writeSync(fd, "* [Description](#description)\n")
@@ -94,7 +98,7 @@ function createTableContents(fd) {
     fs.writeSync(fd, "* [Usage Information](#usage-information)\n")
     fs.writeSync(fd, "* [Contribution Guidelines](#contribution-guidelines)\n")
     fs.writeSync(fd, "* [Test Instructions](#test-instructions)\n")
-    fs.writeSync(fd, "* [How to Reach Me With Questions](#how-to-reach-me-with-questions)\n")
+    fs.writeSync(fd, "* [How to Reach Me With Questions](#how-to-reach-me-with-questions)\n\n")
 }
 
 function createDescription(fd, description) {
@@ -121,7 +125,7 @@ function createTest(fd, test) {
     fs.writeSync(fd, '## Test Instructions \n\n')
     fs.writeSync(fd, `${test}\n\n`)
 }
-
+//This was a tricky one because it was a list instead of input. I ended up writing the if/else if functions to write different license sections based on choice
 function createLicense(fd, licenses) {
     console.log(licenses)
     if (licenses === "MIT") {
@@ -142,7 +146,7 @@ function createLicense(fd, licenses) {
         fs.writeSync(fd, "https://spdx.org/licenses/ISC.html\n\n")
     }
 }
-
+//this one was a little tricky also because it was 2 answers in one section but i figured it out by just passing both questions into the function
 function createQuestion(fd, githubQuestion, emailQuestion) {
     fs.writeSync(fd, '## How to Reach Me With Questions \n\n')
     fs.writeSync(fd, `${githubQuestion}\n\n`)
